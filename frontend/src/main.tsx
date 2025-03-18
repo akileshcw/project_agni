59df56ce32bebfe5dbd388da06714535cfa36707
Akileswaran <darkness.akil@gmail.com>
2025-03-18 12:10:23 +0530

    updated tanstack query provider and axios

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster richColors />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster richColors />
+    </QueryClientProvider>
   </StrictMode>
 );
