8bf20236cd3060a0d2ce23330de39fb7659fa202
karthikailango <karthikami007@gmail.com>
2025-03-25 22:36:26 +0530

    header and button component update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,23 @@
-import { toast } from "sonner";
-import { Button } from "./components/ui/button";
-import Navbar from "./components/custom/Navbar";
+export function Mybutton(){
+  console.log('ready to log in');
+  return(
+<>
+<button id='register-btn'>Login / Register</button>
+</>
+  );
+}
+
 
 function App() {
   return (
-    <>
-      <header className="relative">
-        <Navbar />
-      </header>
-      <main className="bg-blue-500 flex flex-col items-center justify-center min-h-screen gap-8 overflow-hidden">
-        <h1 className="text-7xl font-bold text-center drop-shadow-2xl">
-          hello world
-        </h1>
-        <Button
-          onClick={() => {
-            toast.success("Do you get the toast?");
-          }}
-          size="lg"
-        >
-          Click me
-        </Button>
-      </main>
+   <>
+   <div id='header'>
+    <div>LOGO</div>
+    <div className='Nav-list'>Home</div>
+    <div className='Nav-list'>Students</div>
+    <div className='Nav-list'>Recruiters</div>
+    <Mybutton/>
+   </div>
     </>
   );
 }
