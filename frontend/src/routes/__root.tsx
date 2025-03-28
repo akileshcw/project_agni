4c74b762fd2268908d2bf6e5d41017be60e3361e
Akileswaran <darkness.akil@gmail.com>
2025-03-28 12:21:36 +0530

    hero section style changes

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,25 +1,19 @@
-import MyButton from "@/components/custom/HeaderSection/LoginButton";
-import Logo from "@/components/custom/HeaderSection/Logo";
-import Navbar from "@/components/custom/HeaderSection/Navbar";
-import HeroSection from "@/components/custom/HeroSection/Hero";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import '@/App.css';
+import "@/App.css";
+import Logo from "@/components/custom/HeaderSection/Logo";
+import Navbar from "@/components/custom/HeaderSection/Navbar";
+import MyButton from "@/components/custom/HeaderSection/LoginButton";
 export const Route = createRootRoute({
   component: () => (
     <>
-    <div className="page">
-       <div className='header'>
+      <div className="header">
         <Logo />
         <Navbar />
         <MyButton />
-       </div>
-       <br></br>
-      
-       </div>
-       
+      </div>
       <Outlet />
-      {/* <TanStackRouterDevtools /> */}
+      <TanStackRouterDevtools />
     </>
   ),
 });
