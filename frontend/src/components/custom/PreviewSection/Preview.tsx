58686af0c27f9cdff83e727aec0543f90d747151
karthikailango <karthikami007@gmail.com>
2025-03-31 20:25:36 +0530

    preview page updated

--- frontend/src/components/custom/PreviewSection/Preview.tsx
+++ frontend/src/components/custom/PreviewSection/Preview.tsx
@@ -1,14 +1,18 @@
 import preview1 from '../../../assets/Agni-desktop.png';
 import preview2 from '../../../assets/Agni-mobile.png';
+import './Preview.css';
 export function Preview()
 {
     return(
         <>
+        <div className='preview-container'>
         <h1>Technology News & Job Listings Preview
         </h1>
         <div className='preview'>
-        <img src={preview1}></img>
-        <img src={preview2}></img>
+        <img  id="image1"src={preview1}></img>
+        <img id="image2" src={preview2}></img>
+        </div>
+        
         </div>
         
         </>
